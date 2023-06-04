658404290f336d939be1cca8a16b9f4b4550c662
Владимир Бизунов <v.bizunov@astondevs.ru>
2023-06-04 18:29:24 +0300

    vse sdelal3

---
+++ src/Lesson_three_method_one.java
@@ -0,0 +1,10 @@
+public class Lesson_three_method_one {
+    public static void main(String[] args) {
+        String name;
+        String job;
+        String email;
+        int phone;
+        int zp;
+        int age;
+    }
+}
