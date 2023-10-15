987bf1a186aa56a58c388acc7a902e540f7320c2
Владимир <bizunowvova@gmail.com>
2023-10-15 15:49:44 +0300

    Registr_on_diary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,7 +22,35 @@ public class Main
 //        WebElement btn = driver.findElement(By.xpath("//a[@href='https://diary.ru/user/login']"));
 
         WebElement btn = (new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='https://diary.ru/user/login']"))));
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='https://diary.ru/user/registration']"))));
         btn.click();
+
+        WebElement login = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='SignupForm[username]']"))));
+        login.click();
+        login.sendKeys("bizunowvova");
+
+        WebElement pass = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='SignupForm[password]']"))));
+        pass.click();
+        pass.clear();
+        pass.sendKeys("qwer1234");
+
+        WebElement mail = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='SignupForm[email]']"))));
+        mail.click();
+        mail.sendKeys("bizunowvova@gmail.com");
+
+        WebElement check = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='checkbox']"))));
+        check.click();
+
+        WebElement btn_reg = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id='signup_btn']"))));
+        btn_reg.click();
+
+        WebElement radio = (new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@value='-1']"))));
+        radio.click();
     }
 }
